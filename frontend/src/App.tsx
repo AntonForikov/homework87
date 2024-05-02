8c129c3f4492e8568991c6ec26ef245db031f1be
Anton Forikov <tohabass@gmail.com>
2024-05-02 15:40:49 +0600

    #HW87 frontend: created add new post form,

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,10 @@
 import Header from './components/Header/Header';
 import Home from './containers/Home/Home';
 import {Route, Routes} from 'react-router-dom';
-import Albums from './containers/Albums/Albums';
-import Tracks from './containers/Tracks/Tracks';
 import Register from './containers/User/Register';
 import Login from './containers/User/Login';
 import Container from '@mui/material/Container';
-import TrackHistory from './containers/Tracks/TrackHistory';
+import AddForm from './containers/NewPostForm/AddForm';
 function App() {
   return (
     <>
@@ -17,9 +15,7 @@ function App() {
         <Container maxWidth='xl'>
           <Routes>
             <Route path='/' element={<Home/>}/>
-            <Route path='/trackHistory' element={<TrackHistory />}/>
-            <Route path='artist/:id' element={<Albums/>}/>
-            <Route path='album/:albumId' element={<Tracks/>}/>
+            <Route path='/newPost' element={<AddForm />}/>
             <Route path='/register' element={<Register />}/>
             <Route path='/login' element={<Login />}/>
             <Route path="*" element={<h1>Not found</h1>}/>
