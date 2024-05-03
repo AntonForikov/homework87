e90b0a61c0088e4b0b82f74c8fa31ec7f2e10228
Anton Forikov <tohabass@gmail.com>
2024-05-03 22:44:43 +0600

    #HW87 backend: create fixtures

--- backend/fixtures.ts
+++ backend/fixtures.ts
@@ -1,8 +1,8 @@
 import mongoose from 'mongoose';
 import config from './config';
-import Artist from './models/artist';
-import Album from './models/album';
-import Track from './models/track';
+import User from './models/user';
+import Post from './models/post';
+import Comment from './models/comment';
 
 const dropCollections = async (db: mongoose.Connection, collectionName: string) => {
   try {
@@ -12,7 +12,7 @@ const dropCollections = async (db: mongoose.Connection, collectionName: string) 
   }
 };
 
-const collections = ['albums', 'artists', 'tracks', 'trackhistories', 'users'];
+const collections = ['posts', 'comments', 'users'];
 
 const resetDB = async () => {
   await mongoose.connect(config.mongoose.db);
@@ -20,169 +20,58 @@ const resetDB = async () => {
 
   for (const collection of collections) await dropCollections(db, collection);
 
-  const [artist1, artist2] = await Artist.create(
+  const [user1, user2] = await User.create(
     {
-      name: 'Tool',
-      image: 'fixtureImages/tool_artist.jpeg'
+      username: 'user1',
+      password: '1',
+      token: crypto.randomUUID()
     },
     {
-      name: 'Pantera',
-      image: null
+      username: 'user2',
+      password: '2',
+      token: crypto.randomUUID()
     }
   );
 
-  const [album1, album2, album3, album4] = await Album.create(
+  const [post1, post2] = await Post.create(
     {
-      title: 'aenima',
-      artist: artist1,
-      year: 1996,
-      image: null
+      user: user1._id,
+      title: 'Title for first post',
+      description: null,
+      image: 'fixtureImages/tool_artist.jpeg',
+      date: new Date()
     },
     {
-      title: '10000 Days',
-      artist: artist1,
-      year: 2006,
-      image: null
-    },
-    {
-      title: 'Vulgar Display of Power',
-      artist: artist2,
-      year: 1992,
-      image: 'fixtureImages/vulgar_display_of_power.jpeg'
-    },
-    {
-      title: 'Far Beyond Driven',
-      artist: artist2,
-      year: 1994,
-      image: null
+      user: user2._id,
+      title: 'Title for second post',
+      description: 'Description for second post',
+      image: null,
+      date: new Date()
     }
-  );
+  )
 
-  await Track.create(
+  await Comment.create(
     {
-      title: 'Stinkfist',
-      album: album1,
-      duration: '5:11',
-      indexNumber: 1
+      user: user1,
+      post: post1,
+      text: 'First Comment from first user'
     },
     {
-      title: 'Eulogy',
-      album: album1,
-      duration: '8:29',
-      indexNumber: 2
+      user: user2,
+      post: post1,
+      text: 'Second Comment from second user'
     },
     {
-      title: 'H.',
-      album: album1,
-      duration: '6:07',
-      indexNumber: 3
+      user: user1,
+      post: post2,
+      text: 'First Comment from first user'
     },
     {
-      title: 'Forty Six & 2',
-      album: album1,
-      duration: '8:29',
-      indexNumber: 5
-    },
-    {
-      title: 'Hooker With a Penis',
-      album: album1,
-      duration: '4:34',
-      indexNumber: 7
-    },
-
-    {
-      title: 'Vicarious',
-      album: album2,
-      duration: '7:06',
-      indexNumber: 1
-    },
-    {
-      title: 'Jambi',
-      album: album2,
-      duration: '7:28',
-      indexNumber: 2
-    },
-    {
-      title: 'Wings for Marie',
-      album: album2,
-      duration: '6:11',
-      indexNumber: 3
-    },
-    {
-      title: '10,000 Days',
-      album: album2,
-      duration: '11:13',
-      indexNumber: 4
-    },
-    {
-      title: 'The Pot',
-      album: album2,
-      duration: '6:21',
-      indexNumber: 5
-    },
-
-    {
-      title: 'Mouth for War',
-      album: album3,
-      duration: '3:57',
-      indexNumber: 1
-    },
-    {
-      title: 'A New Level',
-      album: album3,
-      duration: '3:57',
-      indexNumber: 2
-    },
-    {
-      title: 'Walk',
-      album: album3,
-      duration: '5:14',
-      indexNumber: 3
-    },
-    {
-      title: 'Fucking Hostile',
-      album: album3,
-      duration: '2:48',
-      indexNumber: 4
-    },
-    {
-      title: 'This Love',
-      album: album3,
-      duration: '6:32',
-      indexNumber: 5
-    },
-
-    {
-      title: 'Strength Beyond Strength',
-      album: album4,
-      duration: '3:38',
-      indexNumber: 1
-    },
-    {
-      title: 'Becoming',
-      album: album4,
-      duration: '3:05',
-      indexNumber: 2
-    },
-    {
-      title: '5 Minutes Alone',
-      album: album4,
-      duration: '5:47',
-      indexNumber: 3
-    },
-    {
-      title: "I'm Broken",
-      album: album4,
-      duration: '4:24',
-      indexNumber: 4
-    },
-    {
-      title: 'Good Friends and a Bottle of Pills',
-      album: album4,
-      duration: '2:52',
-      indexNumber: 5
-    },
-  );
+      user: user2,
+      post: post2,
+      text: 'Second Comment from second user'
+    }
+  )
 
   await db.close();
 };
